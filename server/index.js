55b756fbcff4e9d69a7c5b4189cd07a9807c94d4
CJ <cjcendana3@gmail.com>
2026-02-20 09:11:52 +0800

    fix: add correct Vercel URL to CORS

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ console.log('');
 app.use(cors({
   origin: [
     process.env.FRONTEND_URL || 'http://localhost:5173',
-    'https://specsmart-anbk.vercel.app',
+    'https://specsmart-n2jd.vercel.app',
     'http://localhost:5173'
   ],
   credentials: true
