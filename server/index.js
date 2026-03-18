fb455cda89d12532bbffda85db29504370cb7816
CJ <cjcendana3@gmail.com>
2026-03-19 07:08:22 +0800

    fix: cors preflight

--- server/index.js
+++ server/index.js
@@ -40,6 +40,8 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
+app.options('*', cors());
+
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
